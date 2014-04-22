bbb9ba9f828f95ccd84d1894a9d12ba1a96d855c
Átila Camurça <camurca.home@gmail.com>
2014-04-22 19:29:04 -0300

    acl preview rule for document.

--- docs/db/dml.acl.sql
+++ docs/db/dml.acl.sql
@@ -33,6 +33,7 @@ INSERT INTO acl_actions(id, action) VALUES (10, 'clone');
 INSERT INTO acl_actions(id, action) VALUES (11, 'view');
 INSERT INTO acl_actions(id, action) VALUES (12, 'save');
 INSERT INTO acl_actions(id, action) VALUES (13, 'ajax-save');
+INSERT INTO acl_actions(id, action) VALUES (14, 'preview');
 
 
 INSERT INTO acl_resources(id, module_id, controller_id, action_id) VALUES (1, 1, 1, 1);
@@ -50,6 +51,7 @@ INSERT INTO acl_resources(id, module_id, controller_id, action_id) VALUES (12, 2
 INSERT INTO acl_resources(id, module_id, controller_id, action_id) VALUES (13, 2, 7, 1);
 INSERT INTO acl_resources(id, module_id, controller_id, action_id) VALUES (14, 2, 7, 12);
 INSERT INTO acl_resources(id, module_id, controller_id, action_id) VALUES (15, 1, 3, 13);
+INSERT INTO acl_resources(id, module_id, controller_id, action_id) VALUES (16, 1, 3, 14);
 
 
 INSERT INTO acl_privileges(resource_id, role_id, allow) VALUES (1, 1, true);
@@ -67,3 +69,4 @@ INSERT INTO acl_privileges(resource_id, role_id, allow) VALUES (12, 3, true);
 INSERT INTO acl_privileges(resource_id, role_id, allow) VALUES (13, 3, true);
 INSERT INTO acl_privileges(resource_id, role_id, allow) VALUES (14, 3, true);
 INSERT INTO acl_privileges(resource_id, role_id, allow) VALUES (15, 2, true);
+INSERT INTO acl_privileges(resource_id, role_id, allow) VALUES (16, 2, true);
